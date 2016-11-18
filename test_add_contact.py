b0ced8aa794d2b153e9e6e7c72415d9f357af194
IrishaS <silkinai@mail.ru>
2016-11-18 23:14:56 +0300

    Выделение параметров и методов контакт

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import  unittest
 
 def is_alert_present(wd):
     try:
@@ -16,47 +15,62 @@ class test_add_contact(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_test_add_contact(self):
-        success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd, firstname="Irina", lastname="Silkina", company="MTS", address="SPb", mobile="+9874561", email="mts@mail.ru", year="2010")
+        self.logout(wd)
+
+    def logout(self, wd):
+         wd.find_element_by_link_text("Logout").click()
+
+    def create_contact(self, wd, firstname, lastname, company, address, mobile, email, year,
+                       day="//div[@id='content']/form/select[1]//option[10]",
+                       month="//div[@id='content']/form/select[2]//option[4]"):
+
+        # init contact creation
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Irina")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Silkina")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("MTS")
+        wd.find_element_by_name("company").send_keys(company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("SPb")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("+1234567")
+        wd.find_element_by_name("mobile").send_keys(mobile)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("mts@mail.ru")
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[10]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[10]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").click()
+        wd.find_element_by_name("email").send_keys(email)
+        if not wd.find_element_by_xpath("%s" % day).is_selected():
+            wd.find_element_by_xpath(day).click()
+        if not wd.find_element_by_xpath(month).is_selected():
+            wd.find_element_by_xpath(month).click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("2000")
+        wd.find_element_by_name("byear").send_keys(year)
+        # submit
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
 
