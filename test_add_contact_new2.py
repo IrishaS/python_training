a3132a75363a90bcd18e4cc832bab28a0f05a1cc
IrishaS <silkinai@mail.ru>
2016-11-21 18:57:51 +0300

    Записан проект PyCharm контакт_версия2

---
+++ test_add_contact_new2.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_contact_new2(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+    
+    def test_test_add_contact_new2(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_contact(wd)
+        self.logout(wd)
+
+    def create_contact(self, wd):
+        # init contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("Irina")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("Silkina")
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("Tele2")
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("SPb")
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("1452367")
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("+79814442233")
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("Tele2@mail.ru")
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("1980")
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys("Moscou")
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
