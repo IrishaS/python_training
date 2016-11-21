a978cf03c37bdc1190df279fecd3f778679d4b32
IrishaS <silkinai@mail.ru>
2016-11-21 15:18:21 +0300

    Перевод с unitest на pytest корректировка
    неправильно была записана функция request.addfinalizer

--- test_add_group.py
+++ test_add_group.py
@@ -8,13 +8,13 @@ from application import Application
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinaliser(fixture.destroy)
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_group(app):
     app.login( username="admin", password="secret")
-    app.create_group( Group(name="aaaadf", header="asdf", footer="asdf"))
+    app.create_group( Group(name="adffff", header="asdf", footer="asdf"))
     app.logout()
 
 def test_add_empty_group(app):
