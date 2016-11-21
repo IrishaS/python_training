9ef991c39cb045bafcc79e59285b92d1c3da440a
IrishaS <silkinai@mail.ru>
2016-11-21 23:22:34 +0300

    Задание 4

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.session.login( username="admin", password="secret")
+    app.session.login(wd="admin", username="admin", password="secret")
     app.group.create(Group(name="adffff", header="asdf", footer="asdf"))
     app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login( username="admin", password="secret")
+    app.session.login(wd="admin", username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
