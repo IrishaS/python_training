4ff06b64110de697d6db6c610d22fb50dcc964aa
IrishaS <silkinai@mail.ru>
2016-11-23 22:51:42 +0300

    Выделен класс для работы с контактами

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,15 +1,11 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
-from fixture.session import SessionHelper
+class ContactHelper:
 
-class Application:
+    def __init__(self,app):
+        self.app = app
 
-    def __init__(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-        self.session = SessionHelper(self)
 
-    def create_contact(self, contact):
-        wd = self.wd
+    def create(self, contact):
+        wd = self.app.wd
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
@@ -45,29 +41,3 @@ class Application:
         wd.find_element_by_name("address2").clear()
         wd.find_element_by_name("address2").send_keys(contact.address2)
         wd.find_element_by_name("submit").click()
-
-
-#    def logout(self):
-#        wd = self.wd
-#        wd.find_element_by_link_text("Logout").click()
-
-
-#    def login(self, username, password):
-#        wd = self.wd
-##        self.app.open_home_page()
-#        self.open_home_page()
-#        wd.find_element_by_name("user").click()
-#        wd.find_element_by_name("user").clear()
-#        wd.find_element_by_name("user").send_keys(username)
-#        wd.find_element_by_name("pass").click()
-#        wd.find_element_by_name("pass").clear()
-#        wd.find_element_by_name("pass").send_keys(password)
-#        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def destroy(self):
-        self.wd.quit()
--- test/test_add_contact_new2.py
+++ test/test_add_contact_new2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from fixture.application1 import Application
+from fixture.application import Application
 from model.contact import Contact
 
 
@@ -13,7 +13,7 @@ def app(request):
 
 def test_test_add_contact_new2(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact( Contact(firstname="Irisha1", lastname="Silkina", company="Tele2", address="SPb", homephone="1452367",
-                            mobile="+79814442233", email="Tele2@mail.ru", day="7", month="6", year="1980", address2="Moscou"))
+    app.contact.create(Contact(firstname="Irisha1", lastname="Silkina", company="Tele2", address="SPb", homephone="1452367",
+                               mobile="+79814442233", email="Tele2@mail.ru", day="7", month="6", year="1980", address2="Moscou"))
     app.session.logout()
 
