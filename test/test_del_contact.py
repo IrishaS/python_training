fd987ae1a33bfac6a08d5b98f3a80d3c5f901c9e
IrishaS <silkinai@mail.ru>
2016-11-28 22:26:09 +0300

    Реализовано предусловие в тесте для удаления контак

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,7 +1,9 @@
-
+from model.contact import Contact
 
 def test_delete_contact(app):
     #app.session.login(username="admin", password="secret")
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.delete_contact()
     #app.session.logout()
 
