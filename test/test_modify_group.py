056b52b04cbb7e890c0b7dfe437c7df44ea01c39
IrishaS <silkinai@mail.ru>
2016-12-01 13:01:19 +0300

    Предусловие в тесте для модификации групп сделано

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,10 +2,15 @@ from model.group import Group
 
 def test_modify_group_name(app):
     #app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="Old group"))
     #app.session.logout()
 
 def test_modify_group_header(app):
-    #app.session.login(username="admin", password="secret")
+          #app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="Old header"))
-    #app.session.logout()
+          #app.session.logout()
+
