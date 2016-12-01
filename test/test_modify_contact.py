f890c3dd7a0cf2bccd245e315ca3f4ed3a1fe9b3
IrishaS <silkinai@mail.ru>
2016-12-01 13:06:56 +0300

    Предусловие в тесте контактов сделано

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,10 +2,14 @@ from model.contact import Contact
 
 def test_modify_contact_firstname(app):
     #app.session.login(username="admin", password="secret")
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="Vadim"))
     #app.session.logout()
 
 def test_modify_contact_lastname(app):
     #app.session.login(username="admin", password="secret")
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(lastname="Ivanov"))
     #app.session.logout()
