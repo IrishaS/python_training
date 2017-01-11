69a3c25402cede59b4dcc4767e0ace302c7d5056
IrishaS <silkinai@mail.ru>
2017-01-12 00:20:54 +0300

    Удаление и модификация случайного элемента

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,29 +1,32 @@
 from model.contact import Contact
+from random import randrange
 
 def test_modify_contact_firstname(app):
-    old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    contact = Contact(firstname="Vadim")
-    contact.id = old_contacts[0].id
-    contact.lastname = old_contacts[0].lastname
-    app.contact.modify_first_contact(contact)
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact = Contact(firstname="d2r2")
+    contact.id = old_contacts[index].id
+    contact.lastname = old_contacts[index].lastname
+    app.contact.modify_contact_by_index(index,contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_contact_lastname(app):
-    old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
     contact = Contact(lastname="Ivanov")
-    contact.id = old_contacts[0].id
-    contact.firstname = old_contacts[0].firstname
-    app.contact.modify_first_contact(contact)
+    contact.id = old_contacts[index].id
+    contact.firstname = old_contacts[index].firstname
+    app.contact.modify_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
