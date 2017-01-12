0fe03bbecb8ae0445bc7b698bf737b96d8cc8863
IrishaS <silkinai@mail.ru>
2017-01-13 00:38:45 +0300

    Параметризация тестов для создания групп и контактов
    Параметризация тестов  для создания групп и контактов

--- test/test_add_contact_new2.py
+++ test/test_add_contact_new2.py
@@ -3,20 +3,26 @@
 
 #from fixture.application import Application
 from model.contact import Contact
+import pytest
+import random
+import string
 
+def random_string(prefix,maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
 
-#@pytest.fixture
-#def app(request):
-    #fixture = Application()
-    #request.addfinalizer(fixture.destroy)
-    #return fixture
+testdata = [Contact(firstname="",lastname="")]+[
+    Contact(firstname=random_string ("Fname",10), lastname=random_string ("Lname",15))
+    for i in range(5)
+]
 
-def test_test_add_contact_new2(app):
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_test_add_contact_new2(app,contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact (firstname="Irisha1", lastname="Silkina", company="Tele2", address="SPb", homephone="1452367",
-                               mobilephone="+79814442233", email="Tele2@mail.ru",
-                               #day="7", month="6",
-                              year="1980", address2="Moscou")
+#    contact = Contact (firstname="Irisha1", lastname="Silkina", company="Tele2", address="SPb", homephone="1452367",
+#                               mobilephone="+79814442233", email="Tele2@mail.ru",
+#                               #day="7", month="6",
+#                              year="1980", address2="Moscou")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
