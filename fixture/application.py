297c7613a2ea9c11c0e5d35dc2b428651123eeb4
IrishaS <silkinai@mail.ru>
2017-01-13 22:54:12 +0300

    Загрузка информации из конфигурационного файла
    Реализована загрузка информации о тестируемом приложении из
    конфигурационного файла

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser, base_url,username,password):
+    def __init__(self, browser, base_url):
 #        self.wd = WebDriver()
         if browser == "firefox":
             self.wd = webdriver.Firefox()
@@ -22,8 +22,8 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
-        self.username = username
-        self.password = password
+ #       self.username = username
+ #       self.password = password
 
     def is_valid(self):
         try:
